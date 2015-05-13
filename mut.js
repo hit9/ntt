1754ce20de94d179d019b9eb5570439233f47ab2
hit9 <hit9@icloud.com>
2015-05-13 18:15:01 +0800

    Add style and fail config support

--- mut.js
+++ mut.js
@@ -19,13 +19,31 @@
 // License: MIT.
 
 var domain = require('domain');
+var util   = require('util');
 
+// exports
+module.exports = function(name, fn) {
+  units.push([name, fn]);
+  units.next();
+};
+var config = exports.confg = {
+  style: true,    // if output with style
+  bail: false,    // bail on first failure
+};
+
+// internals
 var fails = 0;
 var tests = [];
 var units = [];
+var log   = console.log;
 
-var log = console.log;
-
+function styled(s, name) {
+  if (config.style) {
+    var code = util.inspect.colors[name][0];
+    return util.format('\033[%dm%s\033[0m', code, s);
+  }
+  return s;
+}
 
 function execute(name, fn, cb) {
   var d = domain.create();
@@ -42,7 +60,12 @@ function execute(name, fn, cb) {
     } else {
       msg = e.toString();
     }
-    log(' - %s\n\t%s', name, msg);
+    // logging
+    var flag = styled('-', 'red');
+    var nam_ = styled(name, 'red');
+    var msg_ = styled(msg, 'grey');
+    log(' %s %s\n\t%s', flag, nam_, msg_);
+
     fails ++;
     tests.pending = false;
     tests.next();
@@ -62,10 +85,21 @@ tests.next = function() {
       return;
     }
 
+    if (config.bail && fails > 0)
+      return;
+
+    var start = new Date();
+
     tests.pending = true;
     execute(test[0], test[1], function() {
       // on pass
-      log(' + %s', test[0]);
+      // logging
+      var flag = styled('+', 'green');
+      var nam_ = styled(test[0], 'green');
+      var span = styled((new Date() - start).toFixed(2) + 'ms',
+                         'grey');
+      log(' %s %s %s', flag, nam_, span);
+      // roll next
       tests.pending = false;
       tests.next();
     });
@@ -77,20 +111,15 @@ units.next = function() {
   if (!units.pending) {
     var unit = units.shift();
     if (!unit) {
-      log('\n=> %d fails', fails);
+      log(styled('=> %d fails', fails? 'red' : 'green'), fails);
       process.exit(fails);
     }
 
-    log('# %s', unit[0]);
+    log('# %s', styled(unit[0], 'bold'));
     units.pending = true;
     unit[1](function(name, fn) {
       tests.push([name, fn]);
       process.nextTick(tests.next);
     });
   }
-};
-
-module.exports = function(name, fn) {
-  units.push([name, fn]);
-  units.next();
 };
